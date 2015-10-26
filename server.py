bd3bd3bdbb1f9b44c44b6a1169ad22a0ab4c921b
carlos escudero garcia <cescuder@zeta40.aulas.gsyc.es>
2015-10-26 17:59:07 +0100

    server.py

--- server.py
+++ server.py
@@ -11,19 +11,26 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    
+    dict={}
+    
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        print(self.client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
+            linea = line.decode('utf_8')
             print("El cliente nos manda " + line.decode('utf-8'))
 
             # Si no hay más líneas salimos del bucle infinito
-            if not line:
+            if not line or len(linea):
                 break
+        ip = self.client_address[0]
+        registro = linea.split()[0]
+        direccionSIP = linea.split()[1]
+        self.dict[direccionSIP]= ip
+        print (self.dict)
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
