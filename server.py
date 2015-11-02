f4b47cfc7104c213273436185947787ea3ed3086
carlos escudero garcia <cescuder@zeta40.aulas.gsyc.es>
2015-11-02 10:08:50 +0100

    punto 7 terminado

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import json
+import csv
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
@@ -22,21 +24,28 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         direccionSIP = linea.split()[1]
         expires = int(linea.split()[4])
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             print("El cliente nos manda " + line.decode('utf-8'))
-            self.Dicc[direccionSIP]= ip
+            if registro == "REGISTER":
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                self.Dicc[direccionSIP]= ip
             if expires == 0:
-                Dicc.pop(direccionSIP)
+                del self.Dicc[direccionSIP]
 
             # Si no hay más líneas salimos del bucle infinito
             if not line or len(linea):
                 break
                 
 
-        print (self.dicc)
+        print (self.Dicc)
         print (expires)
+        
+    def register2json(self):
+        fich = open(register.json, w)
+        json.dumps((self.Dicc), sort_keys=True, indent=4)
+        
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
